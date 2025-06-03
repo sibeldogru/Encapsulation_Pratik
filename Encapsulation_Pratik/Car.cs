e855608029f2db022f208970aaca72c7d9430a60
sibeldogru <sibeldogru@gmail.com>
2025-06-03 22:18:03 +0300

    Add project files.

---
+++ Encapsulation_Pratik/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Encapsulation_Pratik
+{
+    internal class Car
+    {
+        public void CarInfo()
+        {
+            Console.WriteLine("Arabanızın bilgileri: ");
+            Console.WriteLine("Marka: " + BrandName);
+            Console.WriteLine("Model: " + Model);
+            Console.WriteLine("Renk: " + Color);
+            Console.WriteLine("Kapı sayısı: " + DoorNumber);
+        }
+
+        public string BrandName { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        private int _doorNumber;
+        public int DoorNumber
+        {
+            get
+            {
+                return _doorNumber;
+            }
+
+            set
+            {
+                if (value != 2 && value != 4)
+                {
+                    Console.WriteLine("Geçersiz bir kapı sayısı girdiniz. Kapı sayısı 2 veya 4 olabilir.");
+                    _doorNumber = 0;
+                }
+                else
+                {
+                    _doorNumber = value;
+                }
+            }
+        }
+
+    }
+}
---
+++ Encapsulation_Pratik/Program.cs
@@ -0,0 +1,13 @@
+﻿using Encapsulation_Pratik;
+
+Car car1 = new Car()
+{
+    BrandName = "Ford",
+    Model = "Mustang",
+    Color = "Black",
+    DoorNumber = -1,
+};
+
+car1.CarInfo();
+
+
